7c49da1d92aca9365a5bdd3fdc27454a71e3f9af
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-05-31 18:19:34 -0700

    changed buttons to display:inline-block, added line breaks after divider in search.js

--- client/src/components/pages/search.js
+++ client/src/components/pages/search.js
@@ -84,6 +84,8 @@ class Search extends Component {
                                 <label className='active' htmlFor="zipcode">Enter Your Zipcode:</label>
                             </div>
                             <div className='input-field col s12 divider'></div>
+                            <br />
+                            <br />
                             <div className='input-field col s12'>
                                 <input
                                     onChange={this.onChange}
