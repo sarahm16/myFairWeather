6be02c049799af66dad2638f4e7fd52d4a1a0930
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-05-31 17:33:15 -0700

    removed padding from navbar links, changed li items to divs, centered navbar

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
   constructor() {
     super();
     this.state = {
-      style: {borderBottom: '3px solid teal', paddingLeft: '15px', paddingRight: '15px'}
+      style: {borderBottom: '3px solid teal', paddingLeft: '5px', paddingRight: '5px'}
     }
   }
 
@@ -16,27 +16,29 @@ class Navbar extends Component {
       <div className="navbar-fixed">
         <nav className="z-depth-0 nav-center">
           <div className="nav-wrapper white row">
-            <div className="col s12 l4 brand-logo center black-text">
-              <ul>
-              <li className='tab'><Link to='/' title='home'><i className="material-icons icon-black">home</i></Link></li>              
+            <div className="col s12 l4 brand-logo center black-text links">
+              <div className='col s1'></div>
+
+              <div className='col s2'><Link to='/' title='home'><i className="material-icons icon-black">home</i></Link></div>              
               
-              <li className='tab'>
+              <div className='col s2'>
               {/* {this.props.page !== 'search' && */}
-              <Link to='/search' title='search'><i className="material-icons icon-blue" style={this.props.page === 'search' ? this.state.style : {}}>search</i></Link></li>
+              <Link to='/search' title='search'><i className="material-icons icon-blue" style={this.props.page === 'search' ? this.state.style : {}}>search</i></Link></div>
             
               {/* <li>{this.props.page !== 'favorites' && */}
-              <li>
-              <Link to='/favorites' title='favorites'><i className="material-icons icon-yellow" style={this.props.page === 'favorites' ? this.state.style : {}}>star</i></Link></li>
+              <div className='col s2'>
+              <Link to='/favorites' title='favorites'><i className="material-icons icon-yellow" style={this.props.page === 'favorites' ? this.state.style : {}}>star</i></Link></div>
               
-              <li className='tab'>
+              <div className='col s2'>
               {/* {this.props.page !== 'completed' && */}
-              <Link to='/completed' title='completed'><i className="material-icons icon-green" style={this.props.page === 'completed' ? this.state.style : {}}>check</i></Link></li>
+              <Link to='/completed' title='completed'><i className="material-icons icon-green" style={this.props.page === 'completed' ? this.state.style : {}}>check</i></Link></div>
               
-              
-              <li className='tab'>
+              <div className='col s2'>
               {/* {this.props.page !== 'stats' &&  */}
-              <Link to='/stats' title='stats'><i className="material-icons icon-black" style={this.props.page === 'stats' ? this.state.style : {}}>show_chart</i></Link></li>
-              </ul>
+              <Link to='/stats' title='stats'><i className="material-icons icon-black" style={this.props.page === 'stats' ? this.state.style : {}}>show_chart</i></Link></div>
+              
+              <div className='col s1'></div>
+
             </div>
           </div>
         </nav>
