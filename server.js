ae5ecfed5921090f8250dcf4ed9e5f08dd35e310
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-05-31 17:14:43 -0700

    added remote database and built app

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db,
-    // process.env.MONGODB_URI || "mongodb://fairweather:fa1rweather@ds139956.mlab.com:39956/heroku_z0n9lrbw",
+  .connect(
+    process.env.MONGODB_URI || "mongodb://userdb:userpassword1@ds357708.mlab.com:57708/heroku_sxjd6b94",
 
     { useNewUrlParser: true }
   )
