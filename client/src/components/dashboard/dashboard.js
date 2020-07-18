275a0dc13a766949fc7ddb00ff15d3ddfa9b4d81
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-07-18 11:18:42 -0700

    removed unnecessary code from dashboard.js

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+
 import { logoutUser } from "../../actions/authActions";
 import Button from '../button';
-import {Redirect} from 'react-router-dom';
+
 import './style.css';
 
 class Dashboard extends Component {
@@ -18,7 +19,6 @@ class Dashboard extends Component {
     if(!this.props.auth.isAuthenticated) {
       return <Redirect to='/login' />
     }
-    // const { user } = this.props.auth;
 
     return (
       <div style={{ height: "75vh" }} className="container valign-wrapper">
@@ -26,11 +26,9 @@ class Dashboard extends Component {
           <div className='row'>
           </div>
           <div className="col s12 center-align" id='heading'>
-                {/* <b className='white-text'>Hey, {user.name.split(" ")[0]}</b> */}
-                {/* <h2 className="flow-text white-text text-darken-1"> */}
-                <h4>
-                  Fair Weather Hiking
-                </h4>
+            <h4>
+              Fair Weather Hiking
+            </h4>
             
             <div className='row'>
               {/* Heroku needs a "Link" tag instead of an "a" tag. Also be sure to change "href" to "to" like below. */}
