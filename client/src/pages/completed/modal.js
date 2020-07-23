c364e8f459dc4a32c89c77258c988f8f8400bf54
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-07-23 12:02:06 -0700

    added upload photos button to trip report that utilizes cloudinary widget to access users photos and camera

--- client/src/pages/completed/modal.js
+++ client/src/pages/completed/modal.js
@@ -3,6 +3,17 @@ import PropTypes from 'prop-types';
 
 import './modal.css';
 
+function openWidget(event) {
+  event.preventDefault();
+  window.cloudinary.openUploadWidget({
+    cloudName: 'sarahm16', 
+    uploadPreset: 'gvezom1v'}, (error, result) => { 
+    if (!error && result && result.event === "success") { 
+      console.log('Done! Here is the image info: ', result.info); 
+    }
+  })
+}
+
 const Modal = ({customClass, show, closeCallback, onChangeCallback, commentText, cancelCallback }) => (
   <div className={`modal ${customClass}`} style={{ display: show ? 'block' : 'none'}}>
     <div className="overlay" onClick={closeCallback}></div>
@@ -23,6 +34,7 @@ const Modal = ({customClass, show, closeCallback, onChangeCallback, commentText,
                 <button className="waves-effect waves-light btn hoverable red accent-3" id="cancel-submit" value="cancel" onClick={cancelCallback}>Cancel</button>
               </div>
               <div className='col m2 l2'></div>
+              <div><button id="upload_widget" className="cloudinary-button" onClick={openWidget}>Upload files</button></div>
             </div>
         </form>
     </div>
