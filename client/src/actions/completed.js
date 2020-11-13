5046e55301fd7b2dd8529cdddba7f00d592c6d8d
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-11-12 17:11:36 -0800

    created findCompleted action that is dispatched to search reducer. Action dispatched when completed container mounts

---
+++ client/src/actions/completed.js
@@ -0,0 +1,20 @@
+import API from '../utils/API';
+
+function findCompleted(id) {
+
+    return {
+        type: 'COMPLETED',
+        payload: id
+    }
+    // return dispatch => {
+    //     API.displayCompleted(id)
+    //         .then(res => {
+    //             dispatch({
+    //                 type: 'COMPLETED',
+    //                 payload: res.data
+    //             })
+    //         })
+    // }
+}
+
+export default findCompleted;
--- client/src/pages/completed/completedContainer.js
+++ client/src/pages/completed/completedContainer.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import Results from '../results';
 import Navbar from '../../components/navbar';
 
-import './style.css'
+import './style.css';
 
-function CompletedContainer() {
+import findCompleted from '../../actions/completed';
+
+function CompletedContainer(props) {
+
+    useEffect(() => {
+        props.findCompleted('testing')
+    }, [])
+
     return <div>
         <div className='row'>
             <Navbar page='completed' />
         </div>    
         <div className='completed index-card-bg'>
             <div className='col s8 offset-s2'>
-                <Results type='completed-hikes'/>
+                {/* <Results type='completed-hikes'/> */}
             </div>
         </div>
     </div>
-    
-
 }
 
-export default CompletedContainer;
+const mapDispatchToProps = dispatch => ({
+    findCompleted: (id) => dispatch(findCompleted(id))
+})
+
+export default connect(
+    null,
+    mapDispatchToProps
+)
+ (CompletedContainer);
