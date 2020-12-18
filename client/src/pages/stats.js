2217988d4cb52bdbc467322ab44a386ec1049d02
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-12-18 11:52:20 -0800

    reinstalled react-chartjs-2

--- client/src/pages/stats.js
+++ client/src/pages/stats.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from "react-redux";
 
-//import { Bar } from 'react-chartjs-2'
+import { Bar } from 'react-chartjs-2'
 
 import Navbar from '../components/navbar/index';
 import API from '../utils/API';
@@ -63,156 +63,156 @@ class Stats extends Component {
             })
     }
 
-    // render() {
-    //     return <div className = "">
-    //         <Navbar page='stats' />
-        
-    //         <div className = "container">
-    //             <div className = "stats">
-
-    //                     <h4 className = "banner">You have completed {this.state.hikeCount} hikes so far!</h4>
-    //                     <h5 className = "banner">Here are your stats:</h5>
-    //                         <div className = "chart">
-    //                         <Bar
-    //                         height ={200}
-    //                         width ={650}
-    //                         data={{
-    //                             labels: ['January', 'February', 'March',
-    //                                 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    //                             datasets: [
-    //                                 {
-    //                                     label: 'Elevation',
-    //                                     backgroundColor: 'rgba(75,192,192,1)',
-                                        
-    //                                     data: this.state.elevation
-    //                                 }
-    //                             ]
-    //                         }}
-    //                         options={{
-    //                             maintainAspectRatio: false,
-    //                             legend: {
-    //                                 display: false,
-    //                             },
-    //                             title: {
-    //                                 display: true,
-    //                                 text: 'Elevation Hiked By Month',
-    //                                 fontColor: "white"
-    //                             },
-    //                             scales: {
-    //                                 xAxes: [{ 
-    //                                     gridLines: {
-    //                                         display: true,
-    //                                     },
-    //                                     ticks: {
-    //                                         fontColor: "white", 
-    //                                     },
-    //                                 }],
-    //                                 yAxes: [{
-    //                                     display: true,
-    //                                     gridLines: {
-    //                                         display: true,
-    //                                         color:"white",
-    //                                     },
-    //                                     ticks: {
-    //                                         fontColor: "white", 
-    //                                     }
-    //                                 }],
-    //                             },
-    //                             tooltips: {
-    //                                 callbacks: {
-    //                                 label: function(tooltipItem) {
-    //                                         return tooltipItem.yLabel;
-    //                                 }
-    //                                 }
-    //                             }
-    //                         }}
-    //                         />
-    //                         </div>
-    //                         <div className = "chart">
-    //                         <p className = "totals">Total Elevation: {this.state.totalElevation} ft</p>
-    //                         <Bar
-    //                         data={{
-    //                             labels: ['January', 'February', 'March',
-    //                                 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    //                             datasets: [
-    //                                 {
-    //                                     backgroundColor: 'rgba(70,191,255,1)',
-    //                                     data: this.state.miles
-    //                                 }
-    //                             ]
-    //                         }}
-    //                         options={{
-    //                             maintainAspectRatio: false,
-    //                             title: {
-    //                                 display: true,
-    //                                 text: 'Miles Hiked By Month',
-    //                                 borderColor: 'rgba(0,0,0,1)',
-    //                                 fontColor:"white",
-                                    
-    //                             },
-    //                             scales: {
-    //                                 xAxes: [{ 
-    //                                     gridLines: {
-    //                                         display: true,
-    //                                     },
-    //                                     ticks: {
-    //                                         fontColor: "white", // this here
-    //                                     },
-    //                                 }],
-    //                                 yAxes: [{
-    //                                     display: true,
-    //                                     gridLines: {
-    //                                         display: true,
-    //                                         color:"white",
-    //                                     },
-    //                                     ticks: {
-    //                                         fontColor: "white", // this here
-    //                                     }
-    //                                 }],
-    //                             },
-    //                             legend: {
-    //                                 display: false
-    //                             },
-    //                             tooltips: {
-    //                                 callbacks: {
-    //                                 label: function(tooltipItem) {
-    //                                         return tooltipItem.yLabel;
-    //                                 }
-    //                                 }
-    //                             }
-    //                         }}
-    //                         />
-    //                         <p className = "totals">Total Miles: {this.state.totalMiles} miles</p>
-    //                     </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // }
-
     render() {
-        console.log(this.state.elevation);
-        let elevationData = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-            series: [this.state.elevation]
-        }
-        let mileData = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-            series: [this.state.miles]
-        }
-        return(
-            <div>
-                <Navbar page='stats' />
+        return <div className = "">
+            <Navbar page='stats' />
+        
+            <div className = "container">
+                <div className = "stats">
 
-                <div><h2 className='banner'>***** Under Construction *****</h2></div>
-                <div className='container graphs'>
-                    <div className='ct-chart'>
-                        <ChartistGraph className='graph' style={{stroke: 'white'}} data={elevationData} type='Bar' />
-                        <ChartistGraph className='graph' data={mileData} type='Bar' />
-                    </div>
+                        <h4 className = "banner">You have completed {this.state.hikeCount} hikes so far!</h4>
+                        <h5 className = "banner">Here are your stats:</h5>
+                            <div className = "chart">
+                            <Bar
+                            height ={200}
+                            width ={650}
+                            data={{
+                                labels: ['January', 'February', 'March',
+                                    'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                                datasets: [
+                                    {
+                                        label: 'Elevation',
+                                        backgroundColor: 'rgba(75,192,192,1)',
+                                        
+                                        data: this.state.elevation
+                                    }
+                                ]
+                            }}
+                            options={{
+                                maintainAspectRatio: false,
+                                legend: {
+                                    display: false,
+                                },
+                                title: {
+                                    display: true,
+                                    text: 'Elevation Hiked By Month',
+                                    fontColor: "white"
+                                },
+                                scales: {
+                                    xAxes: [{ 
+                                        gridLines: {
+                                            display: true,
+                                        },
+                                        ticks: {
+                                            fontColor: "white", 
+                                        },
+                                    }],
+                                    yAxes: [{
+                                        display: true,
+                                        gridLines: {
+                                            display: true,
+                                            color:"white",
+                                        },
+                                        ticks: {
+                                            fontColor: "white", 
+                                        }
+                                    }],
+                                },
+                                tooltips: {
+                                    callbacks: {
+                                    label: function(tooltipItem) {
+                                            return tooltipItem.yLabel;
+                                    }
+                                    }
+                                }
+                            }}
+                            />
+                            </div>
+                            <div className = "chart">
+                            <p className = "totals">Total Elevation: {this.state.totalElevation} ft</p>
+                            <Bar
+                            data={{
+                                labels: ['January', 'February', 'March',
+                                    'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                                datasets: [
+                                    {
+                                        backgroundColor: 'rgba(70,191,255,1)',
+                                        data: this.state.miles
+                                    }
+                                ]
+                            }}
+                            options={{
+                                maintainAspectRatio: false,
+                                title: {
+                                    display: true,
+                                    text: 'Miles Hiked By Month',
+                                    borderColor: 'rgba(0,0,0,1)',
+                                    fontColor:"white",
+                                    
+                                },
+                                scales: {
+                                    xAxes: [{ 
+                                        gridLines: {
+                                            display: true,
+                                        },
+                                        ticks: {
+                                            fontColor: "white", // this here
+                                        },
+                                    }],
+                                    yAxes: [{
+                                        display: true,
+                                        gridLines: {
+                                            display: true,
+                                            color:"white",
+                                        },
+                                        ticks: {
+                                            fontColor: "white", // this here
+                                        }
+                                    }],
+                                },
+                                legend: {
+                                    display: false
+                                },
+                                tooltips: {
+                                    callbacks: {
+                                    label: function(tooltipItem) {
+                                            return tooltipItem.yLabel;
+                                    }
+                                    }
+                                }
+                            }}
+                            />
+                            <p className = "totals">Total Miles: {this.state.totalMiles} miles</p>
+                        </div>
                 </div>
             </div>
-        )
+        </div>
     }
+
+    // render() {
+    //     console.log(this.state.elevation);
+    //     let elevationData = {
+    //         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    //         series: [this.state.elevation]
+    //     }
+    //     let mileData = {
+    //         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    //         series: [this.state.miles]
+    //     }
+    //     return(
+    //         <div>
+    //             <Navbar page='stats' />
+
+    //             <div><h2 className='banner'>***** Under Construction *****</h2></div>
+    //             <div className='container graphs'>
+    //                 <div className='ct-chart'>
+    //                     <ChartistGraph className='graph' style={{stroke: 'white'}} data={elevationData} type='Bar' />
+    //                     <ChartistGraph className='graph' data={mileData} type='Bar' />
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     )
+    // }
 }
 
 Stats.propTypes = {
