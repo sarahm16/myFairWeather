73411d46bfc0cd30574d1c667e14c3721fcad61a
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-04-27 12:23:52 -0700

    added day of the week to weather cards

--- client/src/components/hike/weatherCard/weatherCard.js
+++ client/src/components/hike/weatherCard/weatherCard.js
@@ -1,16 +1,18 @@
 import React from "react";
-//import moment from "moment";
+import moment from "moment";
 import './style.css';
 
 
 function WeatherCard(props) {
+    let date = new Date();
+    console.log(date)
   return(
         <div>
             {props.forecast.map(day =>(
                 <div key={day.dt} className = "col s12 m2 l2">
                     <div className="card">
                         <span className="card-date col s5 m4 l12">{
-                            // moment().add(props.forecast.indexOf(day), 'days').format('ddd')
+                            moment().add(props.forecast.indexOf(day), 'days').format('ddd')
                             }</span>
                         <div className="card-image weather-icon-div col s2 m4 l12">
                         <img src = {`http://openweathermap.org/img/w/`+ day.weather[0].icon + ".png"} alt= "weather-icon"></img>
